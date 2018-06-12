40c53477aea46b65db103e0f5d02ca30322979e9
rainyjune <rainyjune@live.cn>
2018-06-12 15:26:27 +0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+var PACKAGE = require('./package.json');
+var version = PACKAGE.version;
+
+const config = [{
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'rc-time-picker-' + version + '.js',
+    library: 'RCTimePicker',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: 'url-loader?limit=8192&name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('rc-time-picker-' + version + '.css')
+  ],
+  externals: {
+    "react": {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    }
+  }
+}, {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'rc-time-picker-' + version + '.min.js',
+    library: 'RCTimePicker',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: 'url-loader?limit=8192&name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: true,
+      comments: false,
+      beautify: false,
+      mangle: {
+        keep_fnames: true,
+        except: ['$super', '$', 'exports', 'require']
+      }
+    }),
+    new ExtractTextPlugin('rc-time-picker-' + version + '.min.css')
+  ],
+  externals: {
+    "react": {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    }
+  }
+}];
+
+module.exports = config;
